a0bad6c09ee3fcd6919b81b0af8e8eb75df204c4
mdmrk <mariodavo.20@gmail.com>
2026-06-26 19:31:13 +0200

    build: wire Shader/GpuMesh/terrain_shader into render's tests

--- src/render/root.zig
+++ src/render/root.zig
@@ -1,9 +1,15 @@
 pub const Atlas = @import("atlas.zig");
 pub const MeshBuilder = @import("mesh_builder.zig");
 pub const chunk_mesher = @import("chunk_mesher.zig");
+pub const Shader = @import("shader.zig");
+pub const GpuMesh = @import("gpu_mesh.zig");
+pub const terrain_shader = @import("terrain_shader.zig");
 
 test {
     _ = Atlas;
     _ = MeshBuilder;
     _ = chunk_mesher;
+    _ = Shader;
+    _ = GpuMesh;
+    _ = terrain_shader;
 }
